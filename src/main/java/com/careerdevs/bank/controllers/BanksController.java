f71f7af7dfde9519ec1a6bb63444d940a52bf8d9
Tatsiana Maksimuk <okeanoka@gmail.com>
2022-10-30 11:59:19 -0700

    Bank Repository and Controllers added

--- src/main/java/com/careerdevs/bank/controllers/BanksController.java
+++ src/main/java/com/careerdevs/bank/controllers/BanksController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -51,5 +52,47 @@ public class BanksController {
             return new ResponseEntity<>("Bank not found", HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(requestedBank.get(), HttpStatus.OK);
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<?> postOneBankById(@PathVariable Long id, @RequestBody Bank newBankData) {
+        Bank requestedBank = bankRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        //Option 1 trusting no interceptor will modify data in the middle
+        //       return ResponseEntity.ok(bankRepository.save(newBankData)); //newBankData should contain ALL fields
+
+        //Option2 Allows us only change data provided to us, validates data at the same time, only changes data we allow to be changed
+        if (!newBankData.getName().equals("")) { //Key must exist
+            requestedBank.setName(newBankData.getName());
+        }
+        if (newBankData.getPhoneNumber() != null && newBankData.getPhoneNumber().length()>=3) {//key does not need to exist, if it does then cannot be empty
+            requestedBank.setPhoneNumber(newBankData.getPhoneNumber());
+        }
+        return ResponseEntity.ok(bankRepository.save(requestedBank));
+    }
+
+    //Don't ever put delete All
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteBankById(@PathVariable Long id) {
+        //If return is unwanted, the below line is negligible - deleteById only fails if void is provided and that cannot be due to path.
+        Bank requestedBank = bankRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        bankRepository.deleteById(id);
+        return ResponseEntity.ok(requestedBank);
+    }
+
+
+    //Creating custom query
+    @GetMapping("/name/{name}") //adding change to route, otherwise it will try to pass it as ID
+    public ResponseEntity<?> findBankByName(@PathVariable String name) {
+        Bank requestedBank = bankRepository.findByName(name).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+        );
+        return new ResponseEntity<>(requestedBank, HttpStatus.OK);
+    }
+
+    @GetMapping("/areaCode/{areaCode}")
+    public ResponseEntity<?> findAllBanksByAreaCode(@PathVariable String areaCode){
+        return new ResponseEntity<>(bankRepository.findAllAreaCodes(areaCode), HttpStatus.OK);
     }
 }
--- src/main/java/com/careerdevs/bank/repositories/BankRepository.java
+++ src/main/java/com/careerdevs/bank/repositories/BankRepository.java
@@ -2,6 +2,15 @@ package com.careerdevs.bank.repositories;
 
 import com.careerdevs.bank.models.Bank;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface BankRepository extends JpaRepository <Bank, Long> {
+import java.util.List;
+import java.util.Optional;
+
+public interface BankRepository extends JpaRepository<Bank, Long> {
+    //Because Name has a field in the model of Bank we can tell MySQL to query for it
+    Optional<Bank> findByName(String name);
+
+    @Query("SELECT b FROM Bank b WHERE b.phoneNumber LIKE ?1%") //1 stands in for our 1st var below
+    List<Bank> findAllAreaCodes(String areaCode);
 }
